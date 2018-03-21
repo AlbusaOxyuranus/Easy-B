507a204857ebcc9cbc67084707bc92f8ab47eb84
Denis Prokhorchik <dionis_999@hotmail.com>
2018-03-21 14:16:31 +0300

    first commit

---
+++ Easy-B/Easy-B/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Easy_B
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    // ReSharper disable once InheritdocConsiderUsage
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
